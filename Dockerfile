d5511cb6d25fae3d03c3f94296d1b5627c55bca4
sooj1n <sujin19371@gmail.com>
2024-09-24 17:07:13 +0900

    0.4/model

--- Dockerfile
+++ Dockerfile
@@ -11,6 +11,6 @@ COPY src/mnist/main.py /code/
 COPY run.sh /code/run.sh
 COPY img /code/images
 
-RUN pip install --no-cache-dir --upgrade git+https://github.com/sooj1n/mnist.git@0.1.3
+RUN pip install --no-cache-dir --upgrade git+https://github.com/sooj1n/mnist.git@0.4/model
 
 CMD ["sh", "run.sh"]
--- src/mnist/worker.py
+++ src/mnist/worker.py
@@ -21,6 +21,26 @@ def get_job_img_task():
 
     return r[0]
 
+# 사용자 이미지 불러오기 및 전처리
+def preprocess_image(image_path):
+    img = Image.open(image_path).convert('L')  # 흑백 이미지로 변환
+    img = img.resize((28, 28))  # 크기 조정
+
+    # 흑백 반전
+    # img = 255 - np.array(img)  # 흑백 반전
+    img = np.array(img)
+    
+    img = img.reshape(1, 28, 28, 1)  # 모델 입력 형태에 맞게 변형
+    img = img / 255.0  # 정규화
+    return img
+
+# 예측
+def predict_digit(image_path):
+    img = preprocess_image(image_path)
+    prediction = model.predict(img)
+    digit = np.argmax(prediction)
+    return digit
+
 def prediction(file_path,num):
     sql = """UPDATE image_processing
     SET prediction_result=%s,
@@ -28,8 +48,15 @@ def prediction(file_path,num):
         prediction_time=%s
     WHERE num=%s
     """
-    import random
-    presult = random.randint(0,9)
+
+    import numpy as np
+    from PIL import Image
+    from keras.models import load_model
+    from tensorflow.keras.datasets import mnist
+    
+    model = load_model('home/sujin/code/mnist/note/mnist240924.keras')
+
+    presult = predict_digit(file_path)
     dml(sql,presult, jigeum.seoul.now(), num)
     return presult
 
@@ -45,7 +72,7 @@ def run():
       return 
 
   num = job['num']
-  file_name = job['file_name']
+  file_name = job['file_nimage_processingame']
   file_path = job['file_path']
 
   # STEP 2
