9019a6a0ac2b15eee3c9ce70fe6b3e3d70f1ee7e
sooj1n <sujin19371@gmail.com>
2024-09-25 09:23:45 +0900

    model.py

---
+++ src/mnist/model.py
@@ -0,0 +1,34 @@
+import numpy as np
+from PIL import Image
+from keras.models import load_model
+import os
+
+def get_model():
+    # 모델 로드
+    model_path = os.getenv('MODEL_PATH', '/home/sujin/code/mnist/note/mnist240924.keras')
+    model = load_model(model_path)
+
+    return model
+
+# 사용자 이미지 불러오기 및 전처리
+def preprocess_image(file_path):
+    img = Image.open(file_path).convert('L')  # 흑백 이미지로 변환
+    img = img.resize((28, 28))  # 크기 조정
+
+    # 흑백 반전
+    # img = 255 - np.array(img)  # 흑백 반전
+    img = np.array(img)
+
+    img = img.reshape(1, 28, 28, 1)  # 모델 입력 형태에 맞게 변형
+    img = img / 255.0  # 정규화
+
+    return img
+
+# 예측
+def predict_digit(file_path):
+    model = get_model()
+    img = preprocess_image(file_path)
+    prediction = model.predict(img)
+    digit = np.argmax(prediction)
+
+    return digit
